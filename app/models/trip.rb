97c04061c0dd5f74443efaa7fc6287e69be59a8b
Seth Schwartz <sschwartz214@gmail.com>
2018-07-27 11:00:27 -0600

    WIP

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,8 @@
 class Trip < ApplicationRecord
   has_many :users
   has_many :trails, through: :users
+
+  def total_distance
+    Trail.sum(:distance)
+  end
 end
--- spec/features/user_visits_trip_show_page_spec.rb
+++ spec/features/user_visits_trip_show_page_spec.rb
@@ -17,4 +17,21 @@ describe 'as a visitor' do
       expect(page).to have_content(trail_1.length)
     end
   end
+  describe "when I visit /trip" do
+    it 'displays a total distance of all trails' do
+      trip_1 = Trip.create!(name: "boulder", start_date: "1/1/11", end_date: "1/3/11")
+      trip_2 = Trip.create!(name: "denver", start_date: "1/2/11", end_date: "1/4/11")
+
+      trail_1 = Trail.create!(name: "rocky", length: 10, address: "a street")
+      trail_2 = Trail.create!(name: "muddy", length: 20, address: "b drive")
+      user = User.create(name: "seth", trip: trip_1, trail: trail_1)
+
+      visit trip_path(trip_1)
+
+      expect(page).to have_content(trail_1.name)
+      expect(page).to have_content(trail_1.address)
+      expect(page).to have_content(trail_1.length)
+    end
+  end
+
 end
